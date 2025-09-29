867a0d8deb126a21a1e084e861441d29dad08a4c
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-09-29 11:13:46 -0400

    update

--- updatedata.sh
+++ updatedata.sh
@@ -10,7 +10,7 @@ curl -s https://api.github.com/repos/AerialiteLabs/sh1mmer/releases/tags/v2.0.0 
 
 curl -s https://api.github.com/repos/AerialiteLabs/sh67mmer/releases/tags/v67 -o src/data/sh67mmer.json
 
-curl -s https://api.github.com/repos/ading2210/shimboot/releases/tags/v1.0.0 -o src/data/shimboot.json
+curl -s https://api.github.com/repos/ading2210/shimboot/releases/tags/v1.3.0 -o src/data/shimboot.json
 
 curl -s https://api.github.com/repos/AerialiteLabs/kvs/releases/tags/v2.0.0 -o src/data/kvs.json
 
