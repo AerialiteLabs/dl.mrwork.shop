e337dc304ff57112bdee628940685c38e69dc476
kxtzownsu <kxtzownsu@gmail.com>
2025-10-05 13:20:27 -0400

    replace astro with vanilla HTML+JS
    Signed-off-by: kxtzownsu <kxtzownsu@gmail.com>

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 A download server built off astro that downloads files from github/nightly while looking like it's happening on-site to the unsuspecting
 
 Oh, there goes Mr. Workshop, There goes Mr. Grim<br>If they gave a prize for best downloads the winner would be him
+
+## running server
+requirements: `python3`
+
+run this to run server (literally so ez)
+```bash
+git clone https://github.com/AerialiteLabs/dl.mrwork.shop
+cd dl.mrwork.shop
+python3 server.py
+```
+
+## credits
+sophie - writing original
+kxtzownsu - rewrite in vanilla HTML
--- updatedata.sh
+++ updatedata.sh
@@ -2,19 +2,17 @@
 
 atag="v$(curl -s https://raw.githubusercontent.com/AerialiteLabs/Aurora/refs/heads/alpine/rootfs/usr/share/aurora/functions | grep 'VERSION\["NUMBER"]="' | cut -d'=' -f2 | tr -d '\"')"
 
-curl -s https://api.github.com/repos/AerialiteLabs/Aurora/releases/tags/$atag -o src/data/aurora.json
+curl -s https://api.github.com/repos/AerialiteLabs/Aurora/releases/tags/$atag -o ./data/aurora.json
 
-curl -s https://api.github.com/repos/AerialiteLabs/sh1mmer/releases/tags/v1.0.0 -o src/data/nanoshims.json
+curl -s https://api.github.com/repos/AerialiteLabs/sh1mmer/releases/tags/v1.0.0 -o ./data/nanoshims.json
 
-curl -s https://api.github.com/repos/AerialiteLabs/sh1mmer/releases/tags/v2.0.0 -o src/data/sh1mmer.json
+curl -s https://api.github.com/repos/AerialiteLabs/sh1mmer/releases/tags/v2.0.0 -o ./data/sh1mmer.json
 
-curl -s https://api.github.com/repos/AerialiteLabs/sh67mmer/releases/tags/v67 -o src/data/sh67mmer.json
+curl -s https://api.github.com/repos/AerialiteLabs/sh67mmer/releases/tags/v67 -o ./data/sh67mmer.json
 
-curl -s https://api.github.com/repos/ading2210/shimboot/releases/tags/v1.3.0 -o src/data/shimboot.json
-sed -i 's/"name": "v1\.3\.0",/"name": "Shimboot v1.3.0",/' src/data/shimboot.json
+curl -s https://api.github.com/repos/ading2210/shimboot/releases/tags/v1.3.0 -o ./data/shimboot.json
+sed -i 's/"name": "v1\.3\.0",/"name": "Shimboot v1.3.0",/' ./data/shimboot.json
 
-curl -s https://api.github.com/repos/AerialiteLabs/kvs/releases/tags/v2.0.0 -o src/data/kvs.json
+curl -s https://api.github.com/repos/AerialiteLabs/kvs/releases/tags/v2.0.0 -o ./data/kvs.json
 
-curl -s https://api.github.com/repos/AerialiteLabs/picoshim/releases/tags/v1.0.0 -o src/data/picoshim.json
-
-cp src/data/*.json public/data/
+curl -s https://api.github.com/repos/AerialiteLabs/picoshim/releases/tags/v1.0.0 -o ./data/picoshim.json
