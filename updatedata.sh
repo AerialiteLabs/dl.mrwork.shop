7ac9fffe7727eee0aab8c68b587e7170959ae45c
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-10-14 09:14:15 -0400

    a

--- updatedata.sh
+++ updatedata.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 atag="v$(curl -s https://raw.githubusercontent.com/AerialiteLabs/Aurora/refs/heads/alpine/rootfs/usr/share/aurora/functions | grep 'VERSION\["NUMBER"]="' | cut -d'=' -f2 | tr -d '\"')"
+batag="v2.0.0"
 
 curl -s https://api.github.com/repos/AerialiteLabs/Aurora/releases/tags/$atag -o ./data/aurora.json
+
+curl -s https://api.github.com/repos/AerialiteLabs/Aurora/releases/tags/$batag -o ./data/beta-aurora.json
 
 curl -s https://api.github.com/repos/AerialiteLabs/sh1mmer/releases/tags/v1.0.0 -o ./data/nanoshims.json
 
